e20a7266a759ba7322617bf3125232871fd7cff8
Vanessa Neves <113128675+vanessaneves-dev@users.noreply.github.com>
2025-02-27 13:24:25 -0300

    :heavy_check_mark: Desafio 2 entregue

--- src/main/java/com/VanessaNeves/desafio/services/Desafio2.java
+++ src/main/java/com/VanessaNeves/desafio/services/Desafio2.java
@@ -1,4 +1,17 @@
 package main.java.com.VanessaNeves.desafio.services;
 
 public class Desafio2 {
+    public static void executar(int numero) {
+        int a = 0, b = 1, temp;
+        while (b < numero) {
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+        if (b == numero || numero == 0) {
+            System.out.println(numero + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println(numero + " não pertence à sequência de Fibonacci.");
+        }
+    }
 }
